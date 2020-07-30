e26a4bf2cf5e77d97e62e520acf8d05fd264cc4a
Andreas Glöckner <andreas@glockner.se>
2020-07-30 08:19:16 +0200

    Major refactoring. New menu. New Functions. Possibility to adjust servo movement.

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,64 @@
 #include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
-
-//needed for library
-#include <DNSServer.h>
-#include <ESPAsyncWiFiManager.h>
 #include <Servo.h>
 #include <ESP8266mDNS.h>
 #include <ESPAsyncTCP.h>
-#include "ESPAsyncWebServer.h"
+#include <ESPAsyncWebServer.h>
 //for LED status
 #include <Ticker.h>
 #include <Timer.h>
-#include <SPI.h>
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-#include <Adafruit_I2CDevice.h>
+#include <EEPROM.h>
+#include "main.h"
 
 #define OLED_RESET 0  // GPIO0
 
 Ticker ticker;
 Timer timer_show;
+Timer rotateTimer;
+int id_timer_show;
+int id_timer_hide;
 AsyncWebServer server(80);
 Servo targetServo;
-DNSServer dns;
-Adafruit_SSD1306 display(OLED_RESET);
+//Adafruit_SSD1306 display(OLED_RESET);
+boolean snabbskytte_on = false;
+int _eeprom_adr = 0;
 
 
 const String COUNT_DOWN_KEY = "countDown";
 const String SHOW_TARGET_KEY = "showTarget";
+const String HIDE_TARGET_KEY = "hideTarget";
 const int DEFAULT_COUNT_DOWN = 10;
 const int DEFAULT_SHOW_TARGET = 4;
 const uint8_t SERVO_PIN = D3;
-const int SERVO_START_POS = 8;
-const int SERVO_END_POS = 93;
+int SERVO_START_POS = 8;
+int SERVO_END_POS = 93;
 
+void checkParameters(AsyncWebServerRequest *request){
+  int params = request->params();
+  for(int i=0;i<params;i++){
+    AsyncWebParameter* p = request->getParam(i);
+    if(p->isFile()){ //p->isPost() is also true
+    Serial.printf("FILE[%s]: %s, size: %u\n", p->name().c_str(), p->value().c_str(), p->size());
+    } else if(p->isPost()){
+    Serial.printf("POST[%s]: %s\n", p->name().c_str(), p->value().c_str());
+    } else {
+    Serial.printf("GET[%s]: %s\n", p->name().c_str(), p->value().c_str());
+    }
+  }
+}
+
+int readEndPosFromMem(){
+    return EEPROM.read(_eeprom_adr);
+}
+
+int readStartPosFromMem(){
+    return EEPROM.read(_eeprom_adr + 1);
+}
+
+void saveServoPositionsToEeprom(){
+  EEPROM.write(_eeprom_adr, SERVO_END_POS);
+  EEPROM.write(_eeprom_adr+1, SERVO_START_POS);
+  EEPROM.commit();
+}
 
 void tick()
 {
@@ -41,7 +66,7 @@ void tick()
   int state = digitalRead(LED_BUILTIN);  // get the current state of GPIO1 pin
   digitalWrite(LED_BUILTIN, !state);     // set pin to the opposite state
 }
-
+/**
 void showOnDisplay(String text){
   display.clearDisplay();
   // text display tests
@@ -51,137 +76,124 @@ void showOnDisplay(String text){
   display.println(text);
   display.display();
 }
-
-//gets called when WiFiManager enters configuration mode
-void configModeCallback (AsyncWiFiManager *myWiFiManager) {
-  Serial.println("Entered config mode");
-  String ip = WiFi.softAPIP().toString();
-  Serial.println(ip);
-  //if you used auto generated SSID, print it
-  Serial.println(myWiFiManager->getConfigPortalSSID());
-  //entered config mode, make led toggle faster
-  showOnDisplay("Config mode: \n" + ip + "\n" + myWiFiManager->getConfigPortalSSID());
-  ticker.attach(0.2, tick);
-}
-
+*/
 void servoStart(){
+  Serial.println("Servo Start");
   targetServo.write(SERVO_START_POS);
 }
 
 void servoStop(){
+  Serial.println("Servo Stop");
   targetServo.write(SERVO_END_POS);
 }
 
-void showTarget(AsyncWebServerRequest *request){
-  String showFor = request->getParam(SHOW_TARGET_KEY)->value();
-  request->send(200, "text/plain", "ok");
-  servoStop();
-  timer_show.after(showFor.toInt()* 1000, servoStart);
+void setServoPos(int pos){
+  Serial.print("Setting servo pos at: ");
+  Serial.println(pos);
+  if(0<pos || pos < 180){
+    targetServo.write(pos);
+    Serial.println("Servo in new position");
+
+  }
+  Serial.flush();
 }
+
+void showTarget(){
+  servoStop();
+}
+
+void hideTarget(){
+  servoStart();
+}
+
 
 void notFound(AsyncWebServerRequest *request) {
     request->send(404, "text/plain", "Not found");
 }
 
 void defaultPage(AsyncWebServerRequest *request) {
-  String countDown = String(DEFAULT_COUNT_DOWN);
-  String showTarget = String(DEFAULT_SHOW_TARGET);
+  rotateTimer.stop(id_timer_show);
+  rotateTimer.stop(id_timer_hide);
 
-  if(request->hasParam(COUNT_DOWN_KEY)){
-    countDown = request->getParam(COUNT_DOWN_KEY)->value();
-  }
-  if(request->hasParam(SHOW_TARGET_KEY)){
-    showTarget = request->getParam(SHOW_TARGET_KEY)->value();
-  }
-  Serial.println("Showtarget = " + showTarget);
-  Serial.println("countDown = " + countDown);
-  AsyncResponseStream *response = request->beginResponseStream("text/html");
-  response->print("<DOCTYPE html><html><head><title>Pistol Practice</title><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-  response->print("<style>h1 {font-size: 36px; } body {font-size: 24px; font-family: verdata, sans-serif; }#button {width: auto; height: 2em; font-size: 24px; font-family: Verdana, sans-serif;}</style></head><body>");
-  response->print("<h1>Welcome Pistol shooting practice</h1>");
-  response->print("<p>Enter number of seconds for countdown and number of seconds for target to show.</p>");
-  response->print("<form action=\"countDown\"><label for=\"countDown\">Timer sec:</><br/><input type=\"number\" id=\"countDown\" name=\"countDown\" value=\"" + countDown + "\"><br/>");
-  response->print("<label for=\"showTarget\">Show Target sec:</><br/><input type=\"number\" id=\"showTarget\" name=\"showTarget\" value=\"" + showTarget + "\"></br>");
-  response->print("<input type=\"submit\" value=\"Start\" id=\"button\"></form>");
-  response->print("</body></html>");
-  request->send(response);
-}
-
-void countDown(AsyncWebServerRequest *request){
-  String countDown = request->getParam(COUNT_DOWN_KEY)->value();
-  String showFor = request->getParam(SHOW_TARGET_KEY)->value();
-  AsyncResponseStream *response = request->beginResponseStream("text/html");
-  response->print("<!DOCTYPE HTML><html><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-  response->print("<style>p {  text-align: center;  font-size: 60px;  margin-top: 0px;}#button {width: auto; height: 2em; font-size: 24px; font-family: Verdana, sans-serif;}");
-  response->print("</style></head><body><p id=\"demo\"></p>");
-  response->print("<script>");
-  // Set the date we're counting down to
-  response->print("var countDownDate = new Date().getTime()+" + countDown + "000;");
-  // Update the count down every 1 second
-  response->print("var x = setInterval(function() {");
-
-  // Get today's date and time
-  response->print("var now = new Date().getTime();");
-
-  // Find the distance between now and the count down date
-  response->print("var distance = countDownDate - now;");
-
-  // Time calculations for days, hours, minutes and seconds
-
-  response->print("var seconds = Math.floor((distance % (1000 * 60)) / 1000);");
-
-  // Output the result in an element with id="demo"
-  response->print("document.getElementById(\"demo\").innerHTML = seconds;");
-
-  // If the count down is over, write some text
-  response->print("if (distance < 0) {    clearInterval(x);   var xhttp = new XMLHttpRequest(); xhttp.onreadystatechange=function(){ ");
-  response->print("if(this.readyState == 4 && this.status == 200) { document.getElementById(\"demo\").innerHTML=\"SHOOT\"; }}; xhttp.open(\"GET\", \"showTarget?" + SHOW_TARGET_KEY + "=" + showFor + "\", true); xhttp.send();}");
-  response->print("}, 1);");
-  response->print("</script><p><form action='/'><input type=\"submit\" value=\"Shoot again\" id=\"button\"/>");
-  response->print("<input type='hidden' name=\"" + COUNT_DOWN_KEY + "\" value=\"" + countDown + "\"/><input type='hidden' name=\"" + SHOW_TARGET_KEY + "\" value=\"" + showFor + "\"/></form></p></body></html>");
-  request->send(response);
-  Serial.print("Count down (s): ");
-  Serial.println(countDown);
-  Serial.print("Show for (s): ");
-  Serial.println(showFor);
+  String page = FPSTR(MY_HTTP_HEAD);
+  page += FPSTR(HTTP_STYLE);
+  page += FPSTR(HTTP_HEAD_END);
+  page += FPSTR(HTTP_BODY_TEXT);
+  page += FPSTR(HTTP_SHOOT_FORM);
+  page += FPSTR(HTTP_END);
+  request->send(200, "text/html", page);
 
 }
 
+void config(AsyncWebServerRequest *request){
+  String startpos=String(SERVO_START_POS);
+  String endpos=String(SERVO_END_POS);
+  if(request->hasParam("startpos")){
+    startpos = request->getParam("startpos")->value();
+  }
+  if(request->hasParam("endpos")){
+    endpos = request->getParam("endpos")->value();
+  }
+  SERVO_START_POS = startpos.toInt();
+  SERVO_END_POS = endpos.toInt();
+  saveServoPositionsToEeprom();
+  Serial.println("Start pos saved:" + startpos);
+  Serial.println("End pos saved: " + endpos);
+
+  String page = FPSTR(MY_HTTP_HEAD);
+  page+= FPSTR(HTTP_STYLE);
+  page+= FPSTR(HTTP_CONFIG_SCRIPT);
+  page+= FPSTR(HTTP_HEAD_END);
+  page+= FPSTR(HTTP_CONFIG_BODY);
+  page+= FPSTR(HTTP_SERVO_CONFIG_FORM);
+  page+= FPSTR(HTTP_END);
+  page.replace("{{startpos}}", startpos);
+  page.replace("{{endpos}}", endpos);
+  request->send(200, "text/html", page);
+}
+
+void snabbskytte(AsyncWebServerRequest *request){
+  hideTarget();
+  snabbskytte_on = true;
+  String page = FPSTR(MY_HTTP_HEAD);
+  page+= FPSTR(HTTP_STYLE);
+  page+= FPSTR(HTTP_HEAD_END);
+  page+= FPSTR(HTTP_FASTSHOOTING_BODY);
+  page+= FPSTR(HTTP_RETURN_FORM);
+  page+= FPSTR(HTTP_END);
+  id_timer_show = rotateTimer.every(7000, showTarget);
+  id_timer_hide = rotateTimer.every(7000+3000, hideTarget);
+  request->send(200, "text/html", page);
+}
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 64x48)
+  delay(3000);
+  EEPROM.begin(128);
+  //display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 64x48)
 
+  //load start & stop position for servo
+  SERVO_END_POS = readEndPosFromMem();
+  SERVO_START_POS = readStartPosFromMem();
+  Serial.println("Start pos from eeprom: " + SERVO_START_POS);
+  Serial.println("End pos from eeprom: " + SERVO_END_POS);
   //set led pin as output
   pinMode(LED_BUILTIN, OUTPUT);
   // start ticker with 0.5 because we start in AP mode and try to connect
   ticker.attach(0.6, tick);
 
-  //WiFiManager
-  //Local intialization. Once its business is done, there is no need to keep it around
-  AsyncWiFiManager wifiManager(&server,&dns);
+  Serial.print("Configuring access point...");
+  /* You can remove the password parameter if you want the AP to be open. */
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP("shooter1");
 
-  //reset settings - for testing
-  //wifiManager.resetSettings();
-
-  //set callback that gets called when connecting to previous WiFi fails, and enters Access Point mode
-  wifiManager.setAPCallback(configModeCallback);
-
-  //fetches ssid and pass and tries to connect
-  //if it does not connect it starts an access point with the specified name
-  //here  "AutoConnectAP"
-  //and goes into a blocking loop awaiting configuration
-  if (!wifiManager.autoConnect()) {
-    Serial.println("failed to connect and hit timeout");
-    //reset and try again, or maybe put it to deep sleep
-    showOnDisplay("Connection failure");
-    ESP.reset();
-    delay(1000);
-  }
-
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+  Serial.println(WiFi.getAutoConnect());
   //if you get here you have connected to the WiFi
   Serial.println("connected...yeey :)");
-  showOnDisplay("Connected to: \n" + WiFi.SSID() + "\nas \n"+ WiFi.localIP().toString());
+  //showOnDisplay("Connected to: \n" + WiFi.SSID() + "\nas \n"+ WiFi.localIP().toString());
   ticker.detach();
   MDNS.begin("shooter");
   //keep LED on
@@ -189,22 +201,45 @@ void setup() {
   server.begin();
   Serial.println("Http server started.");
   targetServo.attach(SERVO_PIN);
-  targetServo.write(SERVO_END_POS);
   delay(1000);
-  targetServo.write(SERVO_START_POS);
 
 
     server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
         defaultPage(request);
     });
 
-    server.on("/countDown", HTTP_GET, [](AsyncWebServerRequest *request){
-        countDown(request);
-
+    server.on("/config", HTTP_GET, [](AsyncWebServerRequest *request){
+      if(request->hasParam("save_btn")){
+        config(request);
+      } else {
+        request->redirect("/");
+      }
     });
 
-    server.on("/showTarget", HTTP_GET, [](AsyncWebServerRequest * request){
-      showTarget(request);
+    server.on("/setPosition", HTTP_GET, [](AsyncWebServerRequest *request){
+        if(request->hasParam("position")){
+          String value = request->getParam("position")->value();
+          setServoPos(value.toInt());
+        }
+        request->send(200, "text/plain", "ok");
+    });
+
+    server.on("/choice", HTTP_GET, [](AsyncWebServerRequest *request){
+        if(request->hasParam("showTarget")){
+          showTarget();
+          request->redirect("/");
+        }
+        if(request->hasParam("hideTarget")){
+          hideTarget();
+          request->redirect("/");
+        }
+        if(request->hasParam("snabbskytte")){
+          snabbskytte(request);
+        }
+        if(request->hasParam("config")){
+          config(request);
+        }
+
     });
 
     server.onNotFound(notFound);
@@ -216,4 +251,13 @@ void loop(void) {
   //server.handleClient();
   MDNS.update();
   timer_show.update();
+  rotateTimer.update();
+  int show;
+  int hide;
+  if(snabbskytte_on){
+
+  } else {
+    rotateTimer.stop(show);
+    rotateTimer.stop(hide);
+  }
 }
---
+++ src/main.h
@@ -0,0 +1,24 @@
+#ifndef main_h
+#define main_h
+
+#if !defined(ESP8266)
+#include <ESP8266.h>
+#endif
+const char MY_HTTP_HEAD[] PROGMEM            = "<!DOCTYPE html><html lang=\"en\"><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no\"/><title>Pistol shooting</title>";
+//"<style>h1 {font-size: 36px; } body {font-size: 24px; font-family: verdata, sans-serif; }#button {width: auto; height: 2em; font-size: 24px; font-family: Verdana, sans-serif;}</style></head><body>"
+const char HTTP_STYLE[] PROGMEM           = "<style>.c{text-align: center;} div,input{padding:5px;font-size:1em;} input{width:95%;} body{text-align: center;font-family:verdana;} button{border:0;border-radius:0.3rem;background-color:#1fa3ec;color:#fff;line-height:2.4rem;font-size:1.2rem;width:100%;} .q{float: right;width: 64px;text-align: right;} .l{background: url(\"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAALVBMVEX///8EBwfBwsLw8PAzNjaCg4NTVVUjJiZDRUUUFxdiZGSho6OSk5Pg4eFydHTCjaf3AAAAZElEQVQ4je2NSw7AIAhEBamKn97/uMXEGBvozkWb9C2Zx4xzWykBhFAeYp9gkLyZE0zIMno9n4g19hmdY39scwqVkOXaxph0ZCXQcqxSpgQpONa59wkRDOL93eAXvimwlbPbwwVAegLS1HGfZAAAAABJRU5ErkJggg==\") no-repeat left center;background-size: 1em;}</style>";
+const char HTTP_HEAD_END[] PROGMEM        = "</head><body><div style='text-align:left;display:inline-block;min-width:260px;'>";
+const char HTTP_BODY_TEXT[] PROGMEM       = "<h1>Pistol shooting practice</h1><p>Fire away or Chose program</p>";
+const char HTTP_SHOOT_FORM[] PROGMEM      = "<form action=\"choice\"><input type=\"submit\" id=\"showTarget\" name=\"showTarget\" value=\"Visa m&aring;l\"><br/><input type=\"submit\" id=\"hideTarget\" name=\"hideTarget\" value=\"D&ouml;lj m&aring;l\"><br/><input type=\"submit\" id=\"Snabb skytte\" name=\"snabbskytte\" value=\"snabbskytte\"><br/><input type=\"submit\" id=\"Konfigurera\" name=\"config\" value=\"config\"></form>";
+const char HTTP_END[] PROGMEM             = "</div></body></html>";
+
+const char HTTP_FASTSHOOTING_BODY[] PROGMEM = "<h1>Pistol shooting practice</h1><p>Snabbskytte, m&aring;l visning 3 sek, g&ouml;mt i 7sek.</p>";
+const char HTTP_RETURN_FORM[] PROGMEM     = "<form action=\"/\"><input type=\"submit\" id=\"return\" name=\"return\" value=\"Till meny\"/></form>";
+
+const char HTTP_CONFIG_BODY[] PROGMEM     ="<h1>Pistol shooting configuration</h1><p>G&ouml;r inst&auml;llningar och spara</p>";
+//const char HTTP_CONFIG_SCRIPT[] PROGMEM          = "<script>function setStartPos(){ var xhttp = new XMLHttpRequest();xhttp.onreadystatechange = function(){if(this.readyState == 4 && this.status == 200){document.getElementById('label_start').innerHTML = document.getElementById('startpos').value;}}xhttp.open(\"GET\", \"setPosition?position='document.getElementById('startpos').value'\", true); xhttp.send();}</script>";
+const char HTTP_CONFIG_SCRIPT[] PROGMEM          = "<script>function setStartPos(){ var xhttp = new XMLHttpRequest(); xhttp.onreadystatechange=function(){ if(this.readyState==4 && this.status ==200) {document.getElementById('label_start').innerHTML = document.getElementById('startpos').value;}}; xhttp.open(\"GET\", \"setPosition?position=\"+document.getElementById('startpos').value, true); xhttp.send();}function setEndPos(){ var xhttp = new XMLHttpRequest(); xhttp.onreadystatechange=function(){ if(this.readyState==4 && this.status ==200) {document.getElementById('label_end').innerHTML = document.getElementById('endpos').value;}}; xhttp.open(\"GET\", \"setPosition?position=\"+document.getElementById('endpos').value, true); xhttp.send();}</script>";
+
+
+const char HTTP_SERVO_CONFIG_FORM[] PROGMEM = "<form action=\"/config\">Start position<br/><label for\"startpos\" id=\"label_start\">{{startpos}}</label><input type=\"range\" onInput=\"setStartPos()\" id=\"startpos\" name=\"startpos\" value=\"{{startpos}}\" min=\"1\" max=\"180\"/><br/>Stop position<br/><label for\"endpos\" id=\"label_end\">{{endpos}}</label><input type=\"range\" onInput=\"setEndPos()\" id=\"endpos\" name=\"endpos\" value=\"{{endpos}}\" min=\"1\" max=\"180\"/><br/><input type=\"submit\" id=\"save_btn\" name=\"save_btn\" value=\"Spara\"/><input type=\"submit\" id=\"return_btn\" name=\"return_btn\" value=\"&Aring;terg&aring;\"></form>";
+#endif
